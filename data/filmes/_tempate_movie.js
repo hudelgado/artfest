d2264289680ffddb1a76f0406ee2f608650341de
Hugo Delgado <hfd19345@campus.fct.unl.pt>
2018-02-18 05:18:04 +0000

    Update movies template and json data files
    - The template uses handlebars templates
      - Composed of a template file and a helpers functions file

---
+++ data/filmes/_tempate_movie.js
@@ -0,0 +1,46 @@
+var movieTemplateHelper = function () {};
+const baseUrlPath = 'https://www.artfestpatrimonios.pt/'
+
+function _build_url(url) {
+  return `${baseUrlPath}${url}`;
+}
+movieTemplateHelper.register = function (Handlebars) {
+    Handlebars.registerHelper('getImagesObj', function(images) {
+        if (!images || !images.featured || !images.filmagens || !images.estreia) {
+          return '"{}"';
+        }
+        var ret = '[';
+        if (images.featured) {
+          ret += `"${baseUrlPath}${images.featured}"\n`;
+        }
+        if (images.estreia) {
+          ret += images.estreia.map(e => `"${baseUrlPath}${e}"`).join(',\n');
+        }
+        if (images.filmagens) {
+          ret += images.filmagens.map(e => `"${baseUrlPath}${e}"`).join(',\n');
+        }
+        return `${ret}]`;
+    });
+    Handlebars.registerHelper('getMovieDescInHTML', function(description) {
+      return '\n' + description.map(e => `<p>${e}</p>\n`).join('');
+    });
+    Handlebars.registerHelper('joinMovieDesc', function(description) {
+      return Array.prototype.join.call(description, " ");
+    });
+    Handlebars.registerHelper('buildUrl', function(url) {
+      return _build_url(url);
+    });
+    Handlebars.registerHelper('isVimeoVideo', function(url) {
+      return true;
+    });
+    Handlebars.registerHelper('getVimeoVideoID', function(url) {
+      let exp = /https?:\/\/player\.vimeo\.com\/video\/(\d+)/;
+      let results = url.match(exp);
+      if (!results) {
+        return ''
+      }
+      return results[1];
+    });
+};
+
+module.exports = movieTemplateHelper;
